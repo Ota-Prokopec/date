7e06cf8458cdf0004b666f3be53468f2dc04a89b
Ota-Prokopec <otaprokopec@gmail.com>
2025-02-19 17:19:56 +0100

    stage

--- apps/next/src/app/profile/page.tsx
+++ apps/next/src/app/profile/page.tsx
@@ -1,18 +1,28 @@
 'use client'
 
-import { UserProfileCard } from '@/components/Profile/UserProfileCard'
+import {
+  UserProfileCard,
+  UserProfileCardSkeletonLoading,
+} from '@/components/Profile/UserProfileCard'
 import { useGetUserProfileQuery } from '@/graphql/generated/apollo'
 import { Button } from '@repo/ui/components/common/Button'
 import { Center } from '@repo/ui/components/common/Center'
+import { match } from 'ts-pattern'
 
 const ProfilePage = () => {
   const userState = useGetUserProfileQuery({ variables: { userId: undefined } })
 
   return (
     <Center className="w-full h-full">
-      <UserProfileCard data={userState.data}>
-        <Button>Edit profile</Button>
-      </UserProfileCard>
+      {match({ loading: userState.loading })
+        .with({ loading: true }, () => (
+          <UserProfileCardSkeletonLoading></UserProfileCardSkeletonLoading>
+        ))
+        .otherwise(() => (
+          <UserProfileCard data={userState.data?.getUserProfile}>
+            <Button>Edit profile</Button>
+          </UserProfileCard>
+        ))}
     </Center>
   )
 }
